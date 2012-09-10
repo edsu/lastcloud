e58f9b9a28df4b156c69682c1eace35ae0924116
Ed Summers <ehs@pobox.com>
2012-09-10 05:12:44 +0000

    make sure 503 errors do not throw javascript errors

--- js/lastcloud.js
+++ js/lastcloud.js
@@ -1,8 +1,10 @@
 function init() {
   $('input[type="submit"]').click(lastfmArtists);
+  $('input[type="text"]').change(lastfmArtists);
 }
 
 function lastfmArtists() {
+  $("#results").empty();
   var key = "0d5ffce4d06bffa87a0415eddf078cde";
   var username = $('input[name="username"]').val();
   var url = "http://ws.audioscrobbler.com/2.0/?method=library.getartists&format=json&limit=100&api_key=" + key + "&user=" + username;
@@ -12,7 +14,6 @@ function lastfmArtists() {
 function soundcloudUsers(lastfmResponse) {
   var artists = lastfmResponse.artists.artist;   
   var template = $("#artist-template").html();
-  $("ul").empty();
   for (var i in artists) {
     var artist = artists[i];
     artist.id = normalize(artist.name);
@@ -24,10 +25,13 @@ function soundcloudUsers(lastfmResponse) {
 }
 
 function addSoundcloud(response) {
+  if (response.length == 0) {
+    console.log("no results from Soundcloud...");
+    return;
+  }
   for (var j in response) {
     var user = response[j];
     var username = normalize(user.username);
-    console.log("looking for id=" + username);
     var li = $("#" + username)[0];
     var template = $("#soundcloud-template").html();
     if (li) {
